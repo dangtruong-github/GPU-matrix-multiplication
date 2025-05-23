09896d40af0fd0144f3e4f4d061e3b0c3d905aa3
Truong <127490669+dangtruong-github@users.noreply.github.com>
2025-05-23 13:25:53 +0700

    edit test

--- naive.cu
+++ naive.cu
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <cuda_runtime.h>
+#include <iostream>
 
 __global__ void test1_kernel(int* result) {
     *result = 1000;  // CUDA kernel sets value to 0
@@ -13,6 +14,8 @@ int test1() {
     test1_kernel<<<1, 1>>>(d_result);
     cudaMemcpy(&h_result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(d_result);
+
+    std::cout << "h_result: " << h_result << "\n";
 
     return h_result;
 }
