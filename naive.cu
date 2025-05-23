c1c5151155681cc8611b374f210e477dee5b5cd6
Truong <127490669+dangtruong-github@users.noreply.github.com>
2025-05-23 14:14:28 +0700

    revert back

--- naive.cu
+++ naive.cu
@@ -8,15 +8,13 @@ __global__ void test1_kernel(int* result) {
 
 int test1() {
     int* d_result;
-    int h_result = 1000;
+    int h_result;
 
-    /*
     cudaMalloc(&d_result, sizeof(int));
     test1_kernel<<<1, 1>>>(d_result);
     cudaDeviceSynchronize();  // Wait for the kernel to finish
     cudaMemcpy(&h_result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(d_result);
-    */
 
     std::cout << "h_result: " << h_result << "\n";
 
