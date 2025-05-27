81ee8289aa20283b4e73b1b1464412b10182779b
Truong <127490669+dangtruong-github@users.noreply.github.com>
2025-05-27 13:26:27 +0700

    add size testing

--- naive/naive_cuda.cu
+++ naive/naive_cuda.cu
@@ -7,8 +7,23 @@ __global__ void test1_kernel(int* result) {
 }
 
 torch::Tensor matmul(torch::Tensor in1, torch::Tensor in2) {
-    auto result = torch::zeros({1}, torch::device(torch::kCUDA).dtype(torch::kInt32));
-    test1_kernel<<<1, 1>>>(result.data_ptr<int>());
-    cudaDeviceSynchronize();  // ensure completion
+    torch::Tensor in1 = in1.contiguous();
+    torch::Tensor in2 = in2.contiguous();
+
+    const int M = in1.size(0);
+    const int N = in1.size(1);
+    const int K = in2.size(1);
+
+    auto result = torch::zeros({3}, torch::device(torch::kCUDA).dtype(torch::kInt32));
+    
+    // Create a CPU tensor with M, N, K for debugging
+    auto debug_tensor = torch::tensor({M, N, K}, torch::dtype(torch::kInt32));
+
+    // Copy to GPU result tensor
+    result.copy_(debug_tensor.to(torch::kCUDA));
+
+    // test1_kernel<<<1, 1>>>(result.data_ptr<int>());
+    // cudaDeviceSynchronize();  // ensure completion
+    
     return result;
 }
